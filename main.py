94181b12320e6f2a1703adebc7ec2765b0e2bdfb
Raman-Shakya <raman.gwarko15@gmail.com>
2022-11-21 20:28:08 +0545

    added highscore

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from Dino import Dino
 from Cactus import Cactus
 from cloud import Cloud
 
+highscore = int(open("highscore.txt",'r').read())
+
 setting = Settings(width=600, height=400, ground=-130) # instantiating settings
 
 # setting up screen
@@ -24,48 +26,60 @@ Screen.register_shape('images/cloud.gif')
 
 
 
-pen = turtle.Turtle()
+def play():
+    global highscore
 
-score_writer = turtle.Turtle()
-score_writer.hideturtle()
-score_writer.goto(0, setting.height/4)
+    pen = turtle.Turtle()
 
-
-def draw_board(): # just draws a straight line representing ground
-    pen.penup()
-    pen.goto(-setting.width, setting.ground)
-    pen.pendown()
-    pen.goto(setting.width, setting.ground)
+    score_writer = turtle.Turtle()
+    score_writer.hideturtle()
+    score_writer.goto(0, setting.height/4)
 
 
-Screen.listen()
-setting.score = 0
+    def draw_board(): # just draws a straight line representing ground
+        pen.penup()
+        pen.goto(-setting.width, setting.ground)
+        pen.pendown()
+        pen.goto(setting.width, setting.ground)
 
-# instantiating dino and cactus
-dino = Dino(0, 0, setting)
-cactus = Cactus(setting)
-cloud  = Cloud(setting)
 
-draw_board()
+    Screen.listen()
 
-while True:
-    # score stuff
-    setting.score += setting.delta_time
-    score_writer.clear()
-    score_writer.write(int(setting.score*10)) # per second score kinda felt slow LOL
+    setting.score = 0
+    # instantiating dino and cactus
+    dino = Dino(0, 0, setting)
+    cactus = Cactus(setting)
+    cloud  = Cloud(setting)
 
-    # event listeners
-    Screen.onkey(dino.jump, 'Up')
-    Screen.onkey(dino.jump, 'space')
+    draw_board()
 
-    # rendering
-    cloud.render()
-    dino.render()
-    if cactus.render(dino): # cactus.render returns false when dino collides with cactus
-        break
+    while True:
+        # score stuff
+        setting.score += setting.delta_time
+        score_writer.clear()
 
-    # frame rate delays
-    time.sleep(setting.delta_time)
-    Screen.update()
+        highscore = max(highscore, int(setting.score*10))
+        score_writer.write(f"HI {highscore:04}, {int(setting.score*10):04}") # per second score kinda felt slow LOL
 
-Screen.mainloop()
+        # event listeners
+        Screen.onkey(dino.jump, 'Up')
+        Screen.onkey(dino.jump, 'space')
+
+        # rendering
+        cloud.render()
+        dino.render()
+        if cactus.render(dino): # cactus.render returns false when dino collides with cactus
+            break
+
+        # frame rate delays
+        time.sleep(setting.delta_time)
+        Screen.update()
+    open("highscore.txt", "w").write(str(highscore))
+
+if __name__ == "__main__":
+    while True:
+        play()
+        time.sleep(2)
+        Screen.clear()
+        Screen.tracer(0,0)
+    Screen.mainloop()
